e3fd42a46ec1bbe2f38704e88cdb8f626a679ef8
Diogo Oliveira <72983023+Diogoliveira119@users.noreply.github.com>
2022-12-21 19:19:09 +0000

    changed location of some files

--- src/script.js
+++
@@ -1,3 +0,0 @@
-var colors = ['#ff0000', '#00ff00', '#0000ff'];
-var random_color = colors[Math.floor(Math.random() * colors.length)];
-document.getElementById('color').style.background = random_color;
